9251e13da8d49f3f5b17b27710c0b014f8f6c201
HVA <quan.ho@qlik.com>
2019-10-03 15:45:51 +0200

    refactor(moving-average): refactor moving average modifier

--- src/expression-modifiers/accumulation/index.js
+++ src/expression-modifiers/accumulation/index.js
@@ -1,4 +1,5 @@
 import util from '../../utils/util';
+import helper from './helper';
 import propertyPanelDef from './properties';
 
 const DEFAULT_OPTIONS = {
@@ -14,148 +15,15 @@ const DEFAULT_OPTIONS = {
 
 const maxNumDimensionsSupported = 2;
 
-function simplifyExpression(expression) {
-  const s = expression.trim();
-  const expComp = s.substring(0, 1) === '=' ? s.substring(1).trim() : s;
-  return expComp;
-}
-
-function getDimSortCriterias(dimensions, dimIdx = 0) {
-  const dimension = dimensions[dimIdx];
-  return dimension.qDef.qSortCriterias[0];
-}
-
-function getDimDef(dimensions, dimIdx = 0, libraryItemsProps) {
-  const dimension = dimensions[dimIdx];
-  return dimension.qLibraryId
-    ? libraryItemsProps[dimension.qLibraryId].qDim.qFieldDefs[0]
-    : dimension.qDef.qFieldDefs[0];
-}
-
-function getDimComp(dimensions, dimIdx, libraryItemsProps) {
-  const sortCriterias = getDimSortCriterias(dimensions, dimIdx);
-  const dimDef = getDimDef(dimensions, dimIdx, libraryItemsProps);
-  if (!sortCriterias.qSortByExpression && !sortCriterias.qSortByNumeric && !sortCriterias.qSortByAscii) {
-    return `[${dimDef}]`;
-  }
-  const type = ['Descending', 'Ascending'];
-  const numericComp = sortCriterias.qSortByNumeric ? `(Numeric, ${type[(sortCriterias.qSortByNumeric + 1) / 2]})` : '';
-  const textComp = sortCriterias.qSortByAscii ? `(Text, ${type[(sortCriterias.qSortByAscii + 1) / 2]})` : '';
-  if (sortCriterias.qSortByNumeric && sortCriterias.qSortByAscii) {
-    return `([${dimDef}], ${numericComp}, ${textComp})`;
-  }
-  if (sortCriterias.qSortByNumeric) {
-    return `([${dimDef}], ${numericComp})`;
-  }
-  if (sortCriterias.qSortByAscii) {
-    return `([${dimDef}], ${textComp})`;
-  }
-  return `[${dimDef}]`;
-}
-
-function getNumStepComp(modifier, numDimensions) {
-  const { crossAllDimensions, fullAccumulation, steps } = modifier;
-  if (!fullAccumulation) {
-    return typeof steps === 'number' && !Number.isNaN(steps) ? steps : 6;
-  }
-  return numDimensions === 2 && crossAllDimensions ? 'RowNo(Total)' : 'RowNo()';
-}
-
-function getAboveCompPrefix(modifier, numDimensions) {
-  const { crossAllDimensions } = modifier;
-  return numDimensions === 2 && crossAllDimensions ? 'Above(Total ' : 'Above(';
-}
-
-function getAboveCompSuffix(numStepComp) {
-  return `, 0, ${numStepComp})`;
-}
-
-function getAboveComp(modifier, numDimensions, expComp, numStepComp) {
-  const aboveCompPrefix = getAboveCompPrefix(modifier, numDimensions);
-  const aboveCompSuffix = getAboveCompSuffix(numStepComp);
-  return aboveCompPrefix + expComp + aboveCompSuffix;
-}
-
-function getExpressionComp(modifier, expression) {
-  const { showExcludedValues } = modifier;
-  const expComp = simplifyExpression(expression);
-  return showExcludedValues ? `${expComp} + Sum({1} 0)` : expComp;
-}
-
-function getRangeSumCompPrefix() {
-  return 'RangeSum(';
-}
-
-function getFunctionSuffix() {
-  return ')';
-}
-
-function getRangeSumComp(comp) {
-  const rangeSumPrefix = getRangeSumCompPrefix();
-  const rangeSumSuffix = getFunctionSuffix();
-  return rangeSumPrefix + comp + rangeSumSuffix;
-}
-
-function getAggrComp(comp1, comp2, comp3) {
-  return `Aggr(${comp1}, ${comp2}, ${comp3})`;
-}
-
-function getNumDimensions({ properties, layout }) {
-  return util.getValue(properties, 'qHyperCubeDef.qDimensions', util.getValue(layout, 'qHyperCube.qDimensionInfo', []))
-    .length;
-}
-
-function needDimension({ modifier, properties, layout }) {
-  return getNumDimensions({ properties, layout }) === 2 && modifier.accumulationDimension === 0;
-}
-
 export default {
   translationKey: 'properties.modifier.accumulation',
 
-  needDimension,
+  needDimension: helper.needDimension,
 
-  simplifyExpression,
+  extractInputExpression: helper.extractInputExpression,
 
   isApplicable({ properties, layout }) {
-    return getNumDimensions({ properties, layout }) <= maxNumDimensionsSupported;
-  },
-
-  extractInputExpression({
-    outputExpression, modifier, properties, layout, numDimensions,
-  }) {
-    if (!modifier) {
-      return;
-    }
-    let numberOfDims;
-    if (typeof numDimensions === 'undefined') {
-      numberOfDims = getNumDimensions({ properties, layout });
-    }
-    const numStepComp = getNumStepComp(modifier, numberOfDims);
-    const aboveCompPrefix = getAboveCompPrefix(modifier, numberOfDims);
-    const rangeSumCompPrefix = getRangeSumCompPrefix();
-    const aggrCompPrefix = needDimension({ modifier, properties, layout }) ? 'Aggr(' : '';
-    const prefix = aggrCompPrefix + rangeSumCompPrefix + aboveCompPrefix;
-    const idx1 = outputExpression.indexOf(prefix);
-    if (idx1 === -1) {
-      return;
-    }
-    const aboveCompSuffix = getAboveCompSuffix(numStepComp);
-    const rangeSumCompSuffix = getFunctionSuffix();
-    const suffix = aboveCompSuffix + rangeSumCompSuffix;
-    const idx2 = outputExpression.lastIndexOf(suffix);
-    if (idx2 === -1) {
-      return;
-    }
-    let exp = outputExpression.substring(idx1 + prefix.length, idx2);
-    const { showExcludedValues } = modifier;
-    if (showExcludedValues) {
-      const expCompSuffix = ' + Sum({1} 0)';
-      if (exp.substring(exp.length - expCompSuffix.length) !== expCompSuffix) {
-        return;
-      }
-      exp = exp.substring(0, exp.length - expCompSuffix.length);
-    }
-    return exp; // eslint-disable-line consistent-return
+    return helper.getNumDimensions({ properties, layout }) <= maxNumDimensionsSupported;
   },
 
   generateExpression({
@@ -166,30 +34,26 @@ export default {
     }
     let numberOfDims = numDimensions;
     if (typeof numberOfDims === 'undefined') {
-      numberOfDims = getNumDimensions({ properties, layout });
+      numberOfDims = helper.getNumDimensions({ properties, layout });
     }
-    const expComp = getExpressionComp(modifier, expression);
-    const numStepComp = getNumStepComp(modifier, numberOfDims);
-    const aboveComp = getAboveComp(modifier, numberOfDims, expComp, numStepComp);
-    const rangeSumComp = getRangeSumComp(aboveComp);
+    const expComp = helper.getExpressionComp(modifier, expression);
+    const numStepComp = helper.getNumStepComp(modifier, numberOfDims);
+    const aboveComp = helper.getAboveComp(modifier, numberOfDims, expComp, numStepComp);
+    const rangeSumComp = helper.getRangeSumComp(aboveComp);
     let generatedExpression = rangeSumComp;
 
-    if (needDimension({ modifier, properties, layout })) {
+    if (helper.needDimension({ modifier, properties, layout })) {
       const dimensions = util.getValue(properties, 'qHyperCubeDef.qDimensions', []);
-      const dim1Comp = getDimComp(dimensions, 1, libraryItemsProps);
-      const dim2Comp = getDimComp(dimensions, 0, libraryItemsProps);
-      const aggrComp = getAggrComp(rangeSumComp, dim1Comp, dim2Comp);
+      const dim1Comp = helper.getDimComp(dimensions, 1, libraryItemsProps);
+      const dim2Comp = helper.getDimComp(dimensions, 0, libraryItemsProps);
+      const aggrComp = helper.getAggrComp(rangeSumComp, dim1Comp, dim2Comp);
       generatedExpression = aggrComp;
     }
     return generatedExpression;
   },
 
   initModifier(modifier) {
-    Object.keys(DEFAULT_OPTIONS).forEach((key) => {
-      if (modifier[key] === undefined) {
-        modifier[key] = DEFAULT_OPTIONS[key]; // eslint-disable-line no-param-reassign
-      }
-    });
+    helper.initModifier(modifier, DEFAULT_OPTIONS);
   },
 
   propertyPanelDef,
--- src/expression-modifiers/moving-average/index.js
+++ src/expression-modifiers/moving-average/index.js
@@ -1,4 +1,6 @@
 import util from '../../utils/util';
+import helper from '../accumulation/helper';
+
 import propertyPanelDef from './properties';
 
 const DEFAULT_OPTIONS = {
@@ -14,53 +16,6 @@ const DEFAULT_OPTIONS = {
 
 const maxNumDimensionsSupported = 2;
 
-function simplifyExpression(expression) {
-  const s = expression.trim();
-  const expComp = s.substring(0, 1) === '=' ? s.substring(1).trim() : s;
-  return expComp;
-}
-
-function getDimSortCriterias(dimensions, dimIdx = 0) {
-  const dimension = dimensions[dimIdx];
-  return dimension.qDef.qSortCriterias[0];
-}
-
-function getDimDef(dimensions, dimIdx = 0, libraryItemsProps) {
-  const dimension = dimensions[dimIdx];
-  return dimension.qLibraryId
-    ? libraryItemsProps[dimension.qLibraryId].qDim.qFieldDefs[0]
-    : dimension.qDef.qFieldDefs[0];
-}
-
-function getDimComp(dimensions, dimIdx, libraryItemsProps) {
-  const sortCriterias = getDimSortCriterias(dimensions, dimIdx);
-  const dimDef = getDimDef(dimensions, dimIdx, libraryItemsProps);
-  if (!sortCriterias.qSortByExpression && !sortCriterias.qSortByNumeric && !sortCriterias.qSortByAscii) {
-    return `[${dimDef}]`;
-  }
-  const type = ['Descending', 'Ascending'];
-  const numericComp = sortCriterias.qSortByNumeric ? `(Numeric, ${type[(sortCriterias.qSortByNumeric + 1) / 2]})` : '';
-  const textComp = sortCriterias.qSortByAscii ? `(Text, ${type[(sortCriterias.qSortByAscii + 1) / 2]})` : '';
-  if (sortCriterias.qSortByNumeric && sortCriterias.qSortByAscii) {
-    return `([${dimDef}], ${numericComp}, ${textComp})`;
-  }
-  if (sortCriterias.qSortByNumeric) {
-    return `([${dimDef}], ${numericComp})`;
-  }
-  if (sortCriterias.qSortByAscii) {
-    return `([${dimDef}], ${textComp})`;
-  }
-  return `[${dimDef}]`;
-}
-
-function getNumStepComp(modifier, numDimensions) {
-  const { crossAllDimensions, fullRange, steps } = modifier;
-  if (!fullRange) {
-    return typeof steps === 'number' && !Number.isNaN(steps) ? steps : 6;
-  }
-  return numDimensions === 2 && crossAllDimensions ? 'RowNo(Total)' : 'RowNo()';
-}
-
 function getDivisorComp(modifier, numDimensions) {
   const { crossAllDimensions, fullRange, steps } = modifier;
   const rowNo = numDimensions === 2 && crossAllDimensions ? 'RowNo(Total)' : 'RowNo()';
@@ -71,71 +26,15 @@ function getDivisorComp(modifier, numDimensions) {
   return rowNo;
 }
 
-function getAboveComp(modifier, numDimensions) {
-  const { crossAllDimensions } = modifier;
-  return numDimensions === 2 && crossAllDimensions ? 'Above(Total ' : 'Above(';
-}
-
-function getExpressionComp(modifier, expression) {
-  const { showExcludedValues } = modifier;
-  const expComp = simplifyExpression(expression);
-  return showExcludedValues ? `${expComp} + Sum({1} 0)` : expComp;
-}
-
-function getNumDimensions({ properties, layout }) {
-  return util.getValue(properties, 'qHyperCubeDef.qDimensions', util.getValue(layout, 'qHyperCube.qDimensionInfo', []))
-    .length;
-}
-
-function needDimension({ modifier, properties, layout }) {
-  return getNumDimensions({ properties, layout }) === 2 && modifier.primaryDimension === 0;
-}
-
 export default {
   translationKey: 'properties.modifier.movingAverage',
 
-  needDimension,
+  needDimension: helper.needDimension,
 
-  simplifyExpression,
+  extractInputExpression: helper.extractInputExpression,
 
   isApplicable({ properties, layout }) {
-    return getNumDimensions({ properties, layout }) <= maxNumDimensionsSupported;
-  },
-
-  extractInputExpression({
-    outputExpression, modifier, properties, layout, numDimensions,
-  }) {
-    if (!modifier) {
-      return;
-    }
-    let numberOfDims;
-    if (typeof numDimensions === 'undefined') {
-      numberOfDims = getNumDimensions({ properties, layout });
-    }
-    const numStepComp = getNumStepComp(modifier, numberOfDims);
-    const aboveComp = getAboveComp(modifier, numberOfDims);
-    const rangeSumCompPrefix = `RangeSum(${aboveComp}`;
-    const rangeSumCompSuffix = `, 0, ${numStepComp}))`;
-    const aggrCompPrefix = needDimension({ modifier, properties, layout }) ? 'Aggr(' : '';
-    const prefix = aggrCompPrefix + rangeSumCompPrefix;
-    const idx1 = outputExpression.indexOf(prefix);
-    if (idx1 === -1) {
-      return;
-    }
-    const idx2 = outputExpression.lastIndexOf(rangeSumCompSuffix);
-    if (idx2 === -1) {
-      return;
-    }
-    let exp = outputExpression.substring(idx1 + prefix.length, idx2);
-    const { showExcludedValues } = modifier;
-    if (showExcludedValues) {
-      const expCompSuffix = ' + Sum({1} 0)';
-      if (exp.substring(exp.length - expCompSuffix.length) !== expCompSuffix) {
-        return;
-      }
-      exp = exp.substring(0, exp.length - expCompSuffix.length);
-    }
-    return exp; // eslint-disable-line consistent-return
+    return helper.getNumDimensions({ properties, layout }) <= maxNumDimensionsSupported;
   },
 
   generateExpression({
@@ -146,34 +45,28 @@ export default {
     }
     let numberOfDims = numDimensions;
     if (typeof numberOfDims === 'undefined') {
-      numberOfDims = getNumDimensions({ properties, layout });
+      numberOfDims = helper.getNumDimensions({ properties, layout });
     }
-    const expComp = getExpressionComp(modifier, expression);
-    const numStepComp = getNumStepComp(modifier, numberOfDims);
-    const aboveComp = getAboveComp(modifier, numberOfDims);
-    const rangeSumComp = `RangeSum(${aboveComp}${expComp}, 0, ${numStepComp}))`;
+    const expComp = helper.getExpressionComp(modifier, expression);
+    const numStepComp = helper.getNumStepComp(modifier, numberOfDims);
+    const aboveComp = helper.getAboveComp(modifier, numberOfDims, expComp, numStepComp);
+    const rangeSumComp = helper.getRangeSumComp(aboveComp);
     const divisorComp = getDivisorComp(modifier, numberOfDims);
     const averageComp = `${rangeSumComp} / ${divisorComp}`;
     let generatedExpression = averageComp;
 
-    if (needDimension({ modifier, properties, layout })) {
+    if (helper.needDimension({ modifier, properties, layout })) {
       const dimensions = util.getValue(properties, 'qHyperCubeDef.qDimensions', []);
-      const aggrComp = `Aggr(${averageComp}, ${getDimComp(dimensions, 1, libraryItemsProps)}, ${getDimComp(
-        dimensions,
-        0,
-        libraryItemsProps,
-      )})`;
+      const dim1Comp = helper.getDimComp(dimensions, 1, libraryItemsProps);
+      const dim2Comp = helper.getDimComp(dimensions, 0, libraryItemsProps);
+      const aggrComp = helper.getAggrComp(rangeSumComp, dim1Comp, dim2Comp);
       generatedExpression = aggrComp;
     }
     return generatedExpression;
   },
 
   initModifier(modifier) {
-    Object.keys(DEFAULT_OPTIONS).forEach((key) => {
-      if (modifier[key] === undefined) {
-        modifier[key] = DEFAULT_OPTIONS[key]; // eslint-disable-line no-param-reassign
-      }
-    });
+    helper.initModifier(modifier, DEFAULT_OPTIONS);
   },
 
   propertyPanelDef,
