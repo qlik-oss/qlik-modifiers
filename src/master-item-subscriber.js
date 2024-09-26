42e9ab8a8b20ff2eba47f29169af30749291d5ee
Tobias Linsefors <T-Wizard@users.noreply.github.com>
2024-09-26 15:47:27 +0200

    fix: ignore missing master items (#370)

--- src/master-item-subscriber.js
+++ src/master-item-subscriber.js
@@ -61,7 +61,7 @@ export default function MasterItemSubscriber({ model, callback }) {
         enabled: false,
         properties: getPropertiesString(itemModel.layout),
       };
-    });
+    }).catch(() => { /* ignore missing library items */ });
   }
 
   return {
